47d2cda997b34fc97841f0020f2cbe973e115463
Van Do Tran <105318189+vantrd@users.noreply.github.com>
2024-10-29 10:19:34 +0000

    update 1

--- main.py
+++ main.py
@@ -1,44 +1,45 @@
 from flask import Flask, request, render_template
-import random
+from random import choice
 
-WORDS = ["apple", "grape", "grade", "melon"]
 
-secret_word = random.choice(WORDS).upper()
+WORDS = ["apple", "maple", "Pneumonoultramicroscopicsilicovolcanoconiosis", "hello"]
+
+secret_word = choice(WORDS).upper()
+
+
 app = Flask(__name__)
 
-@app.route("/",methods = ["GET", "POST"])
+@app.route("/")
 def index():
-    guess = [''] * len(secret_word)
-    if request.method == "POST":
-        guess = [
-            request.form.get(f'letter'{i+1}', '').upper()
-            for i in range(len(secret_word))
-        ]
 
+    return render_template("index.html")
+
+@app.route("/wordle_page", methods = ["GET", "POST"])
+def wordle_page():
+    guess = [""] * len(secret_word)
+    if request.method == "POST":
+        guess = [request.form.get(f"letter{i+1}", "").upper()
+                 for i in range(len(secret_word))]
         guess_word = "".join(guess)
 
-        if len(guess_word) != len(secrete_word):
-            return render_template("index.html", error = "Word must be the correct length", word_length = len(secret_word), guess = guess)
-            feefback = check_guess(guess_word, secret_word)
-
+        if len(guess_word) != len(secret_word):
+            return render_template("wordle_page.html", error = "Not Enough Word", word_length = len(secret_word), guess = guess)
+        feedback = check_guess(guess_word, secret_word)
         if guess_word == secret_word:
-            return render_template("index.html", feedback-feedback, success=True, word_length = len(secret_word), guess=guess)
+            return render_template("wordle_page.html", success = True, feedback = feedback, word_length = len(secret_word), guess = guess)
+        
+        return render_template("wordle_page.html", feedback = feedback, word_length = len(secret_word), guess = guess)
+    return render_template("wordle_page.html",word_length = len(secret_word), guess = guess)
 
-            return render_template("index.html", feedback-feedback, guess=guess, word_length = len(secret_word))
-        return render_template("index.html", word_length = len(secret_word), guess=guess)
-
-    def check_guess(guess_word, secret_word):
-        feedback = []
-        for i in range(len(secret_word)):
-            if guess_word[i] == secret_word[i]:
-                feedback.append("Match")
-            elif guess_word[i] in secret_word:
-                feedback.append("Wrong position")
-            else:
-                feedback.append("Wrong")
-        return feedback
-    if __name__ == "__main__":
-        app.run(debug = True)
-    
-
-
+def check_guess(guess_word, secret_word):
+    feedback = []
+    for i in range(len(guess_word)):
+        if guess_word[i] == secret_word[i]:
+            feedback.append("Correct!")
+        elif guess_word[i] in secret_word:
+            feedback.append("Wrong Position!")
+        else:
+            feedback.append("Incorrect!")
+    return feedback
+if __name__ == "__main__":
+    app.run()
